84a58993eb1a93b8d2f7872fcf60cc20558d988a
Henri Chataing <chataing.henri@gmail.com>
2017-08-23 21:39:30 +0200

    Update main loop with PPU graphics

--- src/Core.cc
+++ src/Core.cc
@@ -1,12 +1,17 @@
 
+#include "type.h"
 #include "Core.h"
 #include "Memory.h"
 #include "M6502State.h"
 #include "M6502Eval.h"
 #include "M6502Jit.h"
-#include "type.h"
+#include "N2C02State.h"
+#include "Events.h"
 
 #include <iostream>
+#include <ctime>
+
+using namespace std;
 
 namespace Core
 {
@@ -16,40 +21,108 @@ namespace Core
  */
 void emulate()
 {
-    M6502::InstructionCache cache;
-    M6502::Instruction *instr;
-
+    M6502::currentState->clear();
     M6502::currentState->reset();
+    /// FIXME remove this line !
+    // M6502::currentState->regs.pc = 0xc000;
 
     try {
-        for (int i = 0; i < 10; i++) {
-            u8 opcode;
-            instr = cache.cacheBlock(M6502::currentState->regs.pc);
-            if (instr) {
-                if (instr->branch)
-                    opcode = instr->opcode;
-                else {
-                    instr->run();
-                    opcode = Memory::load(M6502::currentState->regs.pc);
-                }
-            } else
-                opcode = Memory::load(M6502::currentState->regs.pc);
+        while (true) {
+            uint cycles = M6502::currentState->cycles;
+            /* Catch interrupts */
+            if (M6502::currentState->nmi) {
+                M6502::Eval::triggerNMI();
+                M6502::currentState->nmi = false;
+            }
+            /* Evaluate next instruction */
+            u8 opcode = Memory::load(M6502::currentState->regs.pc);
             M6502::Eval::runOpcode(opcode);
+            cycles = M6502::currentState->cycles - cycles;
+            while (cycles > 0) {
+                N2C02::dot();
+                N2C02::dot();
+                N2C02::dot();
+                cycles--;
+            }
+            /// TODO see if possible to check less often
+            if (Events::isQuit())
+                break;
         }
     } catch (char const * s) {
         std::cerr << "Fatal error: " << s << std::endl;
     }
 }
 
-};
+#if 0
 
-// 8000  78        SEI          A:00 X:00 Y:00 P:24 SP:FD CYC:0
-// 8001  D8        CLD          A:00 X:00 Y:00 P:24 SP:FD CYC:2
-// 8002  A9 10     LDA #$10     A:00 X:00 Y:00 P:24 SP:FD CYC:4
-// 8004  8D 00 20  STA $2000    A:10 X:00 Y:00 P:24 SP:FD CYC:6
-// 8007  A2 FF     LDX #$FF     A:10 X:00 Y:00 P:24 SP:FD CYC:10
-// 8009  9A        TXS          A:10 X:FF Y:00 P:A4 SP:FD CYC:12
-// 800A  AD 02 20  LDA $2002    A:10 X:FF Y:00 P:A4 SP:FF CYC:14
-// 800D  10 FB     BPL $800A    A:00 X:FF Y:00 P:26 SP:FF CYC:18
-// 800A  AD 02 20  LDA $2002    A:00 X:FF Y:00 P:26 SP:FF CYC:21
-// 800D  10 FB     BPL $800A    A:00 X:FF Y:00 P:26 SP:FF CYC:25
+/**
+ * Emulation rountine.
+ */
+void emulate()
+{
+    M6502::InstructionCache cache;
+    clock_t begin, end;
+    double elapsed;
+
+    begin = clock();
+
+    for (int round = 0; round < 10000; round++) {
+        M6502::currentState->clear();
+        M6502::currentState->reset();
+        /// FIXME remove this line !
+        M6502::currentState->regs.pc = 0xc000;
+
+        try {
+            while (M6502::currentState->cycles < 6000) {
+                u8 opcode = Memory::load(M6502::currentState->regs.pc);
+                M6502::Eval::runOpcode(opcode);
+            }
+        } catch (char const * s) {
+            std::cerr << "Fatal error: " << s << std::endl;
+        }
+    }
+
+    end = clock();
+    elapsed = double(end - begin) * 1000.f / CLOCKS_PER_SEC;
+
+    cerr << "Interpreter : " << elapsed << " ms" << endl;
+
+    begin = clock();
+
+    for (int round = 0; round < 10000; round++) {
+        M6502::currentState->clear();
+        M6502::currentState->reset();
+        /// FIXME remove this line !
+        M6502::currentState->regs.pc = 0xc000;
+
+        try {
+            while (M6502::currentState->cycles < 6000) {
+                M6502::Instruction *instr;
+                u8 opcode;
+
+                instr = cache.fetchInstruction(M6502::currentState->regs.pc);
+                if (instr) {
+                    instr->run();
+                } else {
+                    instr = cache.cacheBlock(M6502::currentState->regs.pc);
+                    if (instr)
+                        instr->run();
+                }
+                opcode = Memory::load(M6502::currentState->regs.pc);
+                M6502::Eval::runOpcode(opcode);
+            }
+        } catch (char const * s) {
+            std::cerr << "Fatal error: " << s << std::endl;
+        }
+    }
+
+    end = clock();
+    elapsed = double(end - begin) * 1000.f / CLOCKS_PER_SEC;
+
+    cerr << "Dynamic recompiler : " << elapsed << " ms" << endl;
+    cerr << "Cache size: " << hex << cache.getSize() << endl;
+}
+
+#endif
+
+};
